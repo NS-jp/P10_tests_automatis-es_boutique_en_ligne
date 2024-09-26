e6fd603e652792ff21ae7399c060dceacb1b8415
NozomiS <nozomi.information@gmail.com>
2024-09-26 22:28:42 +0200

    updated smoke test

--- cypress/e2e/smoke.cy.js
+++ cypress/e2e/smoke.cy.js
@@ -1,4 +1,4 @@
-describe ("Testing before login", () =>{
+describe ("Testing on the login page", () =>{
     it ("check for the presence of elements in login page", () => {
         cy.visit('/')
         cy.get('[data-cy="nav-link-login"]').click()
@@ -8,7 +8,7 @@ describe ("Testing before login", () =>{
     }) 
 })
 
-describe ("Tesing after login", ()=> {
+describe ("Tesing on the product page", ()=> {
    beforeEach(()=> {
         cy.login('test2@test.fr', 'testtest')
 
@@ -17,9 +17,20 @@ describe ("Tesing after login", ()=> {
 
 
     it ("check for the presence of add-to-cart button and the product availability field", ()=>{
+        const checkProductDetails = (index, totalProducts)=> {
+            cy.get ('[data-cy="product-link"]').eq(index).click()
+            cy.get ('[data-cy="detail-product-add"]').should('be.visible')
+            cy.get ('[data-cy="detail-product-stock"]').should('be.visible')
+            cy.go('back')
+            if (index + 1 < totalProducts) {
+                checkProductDetails(index + 1, totalProducts )
+            }
+        }
         cy.visit('/#/products')
-        cy.get ('[data-cy="product-link"]').first().click()
-        cy.get ('[data-cy="detail-product-add"]').should('be.visible')
-        cy.get ('[data-cy="detail-product-stock"]').should('be.visible')
+        cy.get ('[data-cy="product-link"]').then (($links) => {
+            const totalProducts = $links.length;
+            checkProductDetails(0, totalProducts)
+        })
     })
+
 })
